5a0719f518c043672427f6ef05884b2a321e1ff0
modelstudent101 <stephenshortall@gmail.com>
2020-06-21 18:06:10 -0700

    Update build.sh

--- Docker/build.sh
+++ Docker/build.sh
@@ -2,6 +2,6 @@
 
 SSH_PRIVATE_KEY="$(cat ~/.ssh/id_rsa)"
 
-docker buildx --platform linux/amd64,linux/arm64,linux/arm/v7 --build-arg SSH_PRIVATE_KEY="$SSH_PRIVATE_KEY" -t withings-garmin .
+docker buildx build --platform linux/amd64,linux/arm64,linux/arm/v7 --build-arg SSH_PRIVATE_KEY="$SSH_PRIVATE_KEY" -t withings-garmin .
 
 docker tag withings-garmin:latest jaroslawhartman/withings-garmin:latest
